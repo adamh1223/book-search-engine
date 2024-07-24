024ab84a79c8ef24f44938030fa22b290dbc23c2
Adamh1223 <adam@hussmedia.io>
2024-07-23 23:38:37 -0700

    get ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./schemas/schema");
 const resolvers = require("./schemas/resolvers");
 
@@ -15,10 +16,21 @@ const startServer = async () => {
   // Apply Apollo GraphQL middleware to the Express app
   server.applyMiddleware({ app, path: "/graphql" });
 
-  // Start the Express server
-  app.listen({ port: 3001 }, () =>
-    console.log(`Server ready at http://localhost:3001${server.graphqlPath}`)
-  );
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+
+  // Start the Express server with the port from environment variables or fallback to 3001
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(
+      `Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 };
 
 // Call the async function to start the server
