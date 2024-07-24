7655b98e0c769e6b10738d2db3534ace2f25adbf
Adamh1223 <adam@hussmedia.io>
2024-07-23 21:58:04 -0700

    updated server code

---
+++ README.md
@@ -0,0 +1,87 @@
+
+```
+book-search-engine
+├─ .git
+│  ├─ HEAD
+│  ├─ config
+│  ├─ description
+│  ├─ hooks
+│  │  ├─ applypatch-msg.sample
+│  │  ├─ commit-msg.sample
+│  │  ├─ fsmonitor-watchman.sample
+│  │  ├─ post-update.sample
+│  │  ├─ pre-applypatch.sample
+│  │  ├─ pre-commit.sample
+│  │  ├─ pre-merge-commit.sample
+│  │  ├─ pre-push.sample
+│  │  ├─ pre-rebase.sample
+│  │  ├─ pre-receive.sample
+│  │  ├─ prepare-commit-msg.sample
+│  │  ├─ push-to-checkout.sample
+│  │  └─ update.sample
+│  ├─ index
+│  ├─ info
+│  │  └─ exclude
+│  ├─ objects
+│  │  ├─ info
+│  │  └─ pack
+│  │     ├─ pack-885b939a95df613031bf43577b36717fd20416eb.idx
+│  │     └─ pack-885b939a95df613031bf43577b36717fd20416eb.pack
+│  ├─ packed-refs
+│  └─ refs
+│     ├─ heads
+│     │  └─ main
+│     ├─ remotes
+│     │  └─ origin
+│     │     └─ HEAD
+│     └─ tags
+├─ .gitignore
+├─ .npmrc
+├─ client
+│  ├─ .eslintrc.cjs
+│  ├─ .gitignore
+│  ├─ index.html
+│  ├─ package.json
+│  ├─ public
+│  │  └─ vite.svg
+│  ├─ src
+│  │  ├─ App.css
+│  │  ├─ App.jsx
+│  │  ├─ assets
+│  │  │  └─ react.svg
+│  │  ├─ components
+│  │  │  ├─ LoginForm.jsx
+│  │  │  ├─ Navbar.jsx
+│  │  │  └─ SignupForm.jsx
+│  │  ├─ main.jsx
+│  │  ├─ pages
+│  │  │  ├─ SavedBooks.jsx
+│  │  │  └─ SearchBooks.jsx
+│  │  └─ utils
+│  │     ├─ API.js
+│  │     ├─ auth.js
+│  │     └─ localStorage.js
+│  └─ vite.config.js
+├─ package.json
+└─ server
+   ├─ .gitignore
+   ├─ config
+   │  └─ connection.js
+   ├─ controllers
+   │  └─ user-controller.js
+   ├─ models
+   │  ├─ Book.js
+   │  ├─ User.js
+   │  └─ index.js
+   ├─ package-lock.json
+   ├─ package.json
+   ├─ routes
+   │  ├─ api
+   │  │  ├─ index.js
+   │  │  └─ user-routes.js
+   │  └─ index.js
+   ├─ server.js
+   └─ utils
+      └─ auth.js
+
+```
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,73 +1,88 @@
-// import user model
-const { User } = require('../models');
-// import sign token function from auth
-const { signToken } = require('../utils/auth');
+const User = require("../models/User");
+const { signToken } = require("../utils/auth");
+const bcrypt = require("bcrypt");
 
-module.exports = {
-  // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
-    const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+// Create a new user
+const createUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
     });
-
-    if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
-    }
-
-    res.json(foundUser);
-  },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
-    const user = await User.create(body);
-
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
-    }
     const token = signToken(user);
     res.json({ token, user });
-  },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
-  async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+// Get a single user
+const getSingleUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
     if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
+      return res.status(404).json({ message: "User not found" });
     }
+    res.json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
-    const correctPw = await user.isCorrectPassword(body.password);
-
-    if (!correctPw) {
-      return res.status(400).json({ message: 'Wrong password!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
-    console.log(user);
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json(err);
-    }
-  },
-  // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
+// Save a book to a user's profile
+const saveBook = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { $push: { savedBooks: req.body } },
       { new: true }
     );
-    if (!updatedUser) {
-      return res.status(404).json({ message: "Couldn't find user with this id!" });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+// Delete a book from a user's profile
+const deleteBook = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { $pull: { savedBooks: { bookId: req.params.bookId } } },
+      { new: true }
+    );
+    res.json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+// Login a user
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "No user found with this email" });
     }
-    return res.json(updatedUser);
-  },
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports = {
+  createUser,
+  getSingleUser,
+  saveBook,
+  deleteBook,
+  login,
 };
